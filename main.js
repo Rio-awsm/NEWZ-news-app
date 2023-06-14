a71bc5ce3597333e8186057769087088e14e8ef3
Supriyo Saha <mm.saha1982@gmail.com>
2023-06-14 14:52:37 +0530

    frontend and api call done

---
+++ main.js
@@ -0,0 +1,14 @@
+const API_KEY = "efb79a997d2f4f129effe110e4b3d64c";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+function reload() {
+    window.location.reload();
+}
+
+async function fetchNews(query){
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    console.log(data);
+}
